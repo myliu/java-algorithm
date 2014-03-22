f0246e4b543560e12fdec85f44fd2d290fd5a324
mliu <mliu@quantcast.com>
2014-03-22 07:58:51 -0700

    BinaryTreePreorderTraversal

---
+++ leet/BinaryTreePreorderTraversal.java
@@ -0,0 +1,32 @@
+package leet;
+
+import java.util.ArrayList;
+
+public class BinaryTreePreorderTraversal {
+
+    // Definition for binary tree
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        preorderTraversal(root, result);
+        return result;
+    }
+
+    public void preorderTraversal(TreeNode root, ArrayList<Integer> result) {
+        if (root == null)
+            return;
+
+        result.add(root.val);
+        preorderTraversal(root.left, result);
+        preorderTraversal(root.right, result);
+    }
+}
