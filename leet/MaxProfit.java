ebe910416f5e2e08de8b87754267f8553ee6ac5f
mliu <mliu@quantcast.com>
2014-03-23 23:53:05 -0700

    MaxProfitII

--- leet/MaxProfit.java
+++ leet/MaxProfit.java
@@ -8,7 +8,7 @@ public class MaxProfit {
     public static void main(String[] args) {
         int[] prices1 = { 3, 2, 6, 5, 0, 3 };
         System.out.println(maxProfit(prices1));
-        
+
         int[] prices2 = { 1, 2, 4 };
         System.out.println(maxProfit(prices2));
     }
@@ -40,5 +40,22 @@ public class MaxProfit {
         }
 
         return b;
+    }
+
+    public int maxProfitII(int[] prices) {
+        if (prices == null)
+            return 0;
+
+        int len = prices.length;
+        if (len <= 1)
+            return 0;
+
+        int profit = 0;
+        for (int i = 0; i < len - 1; i++) {
+            if (prices[i] < prices[i + 1])
+                profit += prices[i + 1] - prices[i];
+        }
+
+        return profit;
     }
 }
